62983d08ea0711aaa849e0cd3b08916c7c9dbd1e
Ibrahim Haroon <ibrahim.haroon03@gmail.com>
2025-01-29 18:31:02 +0000

    Made sure values cannot be null & that only specific values can be entered for certain columns

--- database.sql
+++ database.sql
@@ -2,13 +2,13 @@ CREATE DATABASE strokeData;
 
 CREATE TABLE patientData (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    gender VARCHAR(10),
-    hypertension BOOLEAN,
-    heart_disease BOOLEAN,
-    ever_married BOOLEAN,
-    work_type VARCHAR(20),
-    residence_type VARCHAR(10),
-    avg_glucose_level DECIMAL(5,2),
-    bmi DECIMAL(3,1),
-    smoking_status VARCHAR(20)
-)
+    gender ENUM('Male', 'Female', 'Other') NOT NULL,
+    hypertension BOOLEAN NOT NULL,
+    heart_disease BOOLEAN NOT NULL,
+    ever_married BOOLEAN NOT NULL,
+    work_type ENUM('Children', 'Gov_job', 'Never_worked', 'Private', 'Self-employed') NOT NULL,
+    residence_type ENUM('Urban', 'Rural') NOT NULL,
+    avg_glucose_level DECIMAL(5,2) NOT NULL,
+    bmi DECIMAL(3,1) NOT NULL,
+    smoking_status ENUM('never smoked', 'formerly smoked', 'smoked', 'unknown') NOT NULL
+);
