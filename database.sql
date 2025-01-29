8bc5335d2759d023052861f2338649fab4f3ded6
Ibrahim Haroon <ibrahim.haroon03@gmail.com>
2025-01-29 18:22:53 +0000

    Created columns and set data types

--- database.sql
+++ database.sql
@@ -1,1 +1,14 @@
 CREATE DATABASE strokeData;
+
+CREATE TABLE patientData (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    gender VARCHAR(10),
+    hypertension BOOLEAN,
+    heart_disease BOOLEAN,
+    ever_married BOOLEAN,
+    work_type VARCHAR(20),
+    residence_type VARCHAR(10),
+    avg_glucose_level DECIMAL(5,2),
+    bmi DECIMAL(3,1),
+    smoking_status VARCHAR(20)
+)
