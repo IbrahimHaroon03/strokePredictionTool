3acb3794f5cd3318cd4812e59c54352a113b6857
Ibrahim Haroon <ibrahim.haroon03@gmail.com>
2025-02-06 13:03:21 +0000

    Created a new table to store patient medical info

--- SQL_PHP/database.sql
+++ SQL_PHP/database.sql
@@ -6,3 +6,18 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     role ENUM('admin', 'patient', 'doctor') NOT NULL DEFAULT 'patient' 
 );
+
+CREATE TABLE patientMedicalInfo (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    gender ENUM('Male', 'Female') NOT NULL,
+    age INT CHECK (age >= 0), 
+    hypertension TINYINT(1) CHECK (hypertension IN (0, 1)), 
+    heart_disease TINYINT(1) CHECK (heart_disease IN (0, 1)), 
+    ever_married ENUM('No', 'Yes') NOT NULL,
+    work_type ENUM('children', 'govt_job', 'never_worked', 'private', 'self-employed') NOT NULL,
+    Residence_type ENUM('Rural', 'Urban') NOT NULL,
+    avg_glucose_level DECIMAL(5,2) CHECK (avg_glucose_level >= 0), 
+    bmi DECIMAL(5,2) CHECK (bmi >= 0), 
+    smoking_status ENUM('formerly_smoked', 'never_smoked', 'smokes', 'unknown') NOT NULL,
+    stroke TINYINT(1) CHECK (stroke IN (0, 1))
+)
