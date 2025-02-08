bfe0b4c2c6202e6aa432a4953d5515b747b3145f
Ibrahim Haroon <ibrahim.haroon03@gmail.com>
2025-02-08 20:39:48 +0000

    Added all role access files into one folder

--- templates/all/sign_up.php
+++ templates/all/sign_up.php
@@ -1,23 +1,4 @@
-<?php
-session_start(); // Start the session to check login status
-
-// Check if the user is logged in by verifying session data
-if (isset($_SESSION['user'])) {
-    // Redirect based on the user's role if already logged in
-    $user = $_SESSION['user']; // Get user data from session
-
-    if ($user['role'] == 'admin') {
-        header("Location: ../admin/admin_dashboard.php");
-        exit; // Stop further execution after redirect
-    } elseif ($user['role'] == 'doctor') {
-        header("Location: ../doctor/doctor_dashboard.php");
-        exit;
-    } elseif ($user['role'] == 'patient') {
-        header("Location: ../patient/patient_dashboard.php");
-        exit;
-    }
-}
-?>
+<?php include '../../SQL_PHP/role_access/sign_access.php'; ?>
 
 <!DOCTYPE html>
 <html>
