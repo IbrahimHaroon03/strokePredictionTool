67610751263cc9e4d5ac9c099628f709b674d891
Ibrahim Haroon <ibrahim.haroon03@gmail.com>
2025-02-08 21:34:39 +0000

    Forgot to save changes

---
+++ templates/admin/admin_home.php
@@ -0,0 +1,65 @@
+<?php include '../../SQL_PHP/role_access/sign_access.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../static/navbar_styles.css">
+    <script src="../../static/active.js" defer></script>
+    <title>Document</title>
+</head>
+<body>
+    
+    <!-- Side Navbar -->
+    <nav class="side-navbar">
+        <div class="navbar-title">STROKE PREDICTION TOOL</div> 
+        <ul>
+            <li id="home"><a href="admin_home.php">Home</a></li>
+            <li id="home"><a href="admin_dashboard.php">Dashboard</a></li>
+            <li><a href="../../SQL_PHP/logout.php">Sign Out</a></li>
+        </ul>
+    </nav>
+
+    <!-- Main Content -->
+    <div class="main-content">
+        <section id="section1">
+            <h2>Section 1</h2>
+            <p>This is the content of section 1.</p>
+        </section>
+        <section id="section2">
+            <h2>Section 2</h2>
+            <p>This is the content of section 2.</p>
+        </section>
+        <section id="section3">
+            <h2>Section 3</h2>
+            <p>This is the content of section 3.</p>
+        </section>
+        <section id="section4">
+            <h2>Section 4</h2>
+            <p>This is the content of section 4.</p>
+        </section>
+        <section id="section4">
+            <h2>Section 5</h2>
+            <p>This is the content of section 4.</p>
+        </section>
+        <section id="section4">
+            <h2>Section 6</h2>
+            <p>This is the content of section 4.</p>
+        </section>
+        <section id="section4">
+            <h2>Section 7</h2>
+            <p>This is the content of section 4.</p>
+        </section>
+        <section id="section4">
+            <h2>Section 8</h2>
+            <p>This is the content of section 4.</p>
+        </section>
+        <section id="section4">
+            <h2>Section 9</h2>
+            <p>This is the content of section 4.</p>
+        </section>
+    </div>
+</body>
+
+</html>
