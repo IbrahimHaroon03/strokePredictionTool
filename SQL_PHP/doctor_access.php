4f61e8471e151e36f300851ccc83d1f95b1c5090
Ibrahim Haroon <ibrahim.haroon03@gmail.com>
2025-02-08 15:15:10 +0000

    Added code to try implement role based access

---
+++ SQL_PHP/doctor_access.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    // Redirect to login page if not logged in
+    header("Location: ../templates/login.html");
+    exit();
+}
+
+$allowed_roles = ['doctor']; 
+
+if (!in_array($_SESSION['role'], $allowed_roles)) {
+    // Redirect to a default page if role is not allowed
+    header("Location: ../templates/all/index.html");
+    exit();
+}
+?>
